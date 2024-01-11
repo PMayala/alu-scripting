7430fd105e44a7a46c3b9fe67517fe5355132860
PMayala <p.mayala@alustudent.com>
2024-01-11 03:57:11 -0800

    4-repetition_token_3.rb

--- regular_expressions/4-repetition_token_3.rb
+++ regular_expressions/4-repetition_token_3.rb
@@ -1,19 +1,2 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Please provide an argument."
-else
-  # Get the argument
-  input_string = ARGV[0]
-
-  # Define the regular expression pattern using Oniguruma syntax
-  regex_pattern = /hb(t*n|n)/
-
-  # Match the input against the regular expression
-  if input_string.match?(regex_pattern)
-    puts input_string.scan(regex_pattern).join
-  else
-    puts "No match found."
-  end
-end
+puts ARGV[0].scan(/hbt*n/).join
