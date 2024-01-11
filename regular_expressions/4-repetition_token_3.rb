5320f141dfd6869d69d952b28ee5ecfb3d8d5386
PMayala <p.mayala@alustudent.com>
2024-01-11 03:46:21 -0800

    4-repetition_token_3.rb

--- regular_expressions/4-repetition_token_3.rb
+++ regular_expressions/4-repetition_token_3.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/hb+n/).join
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Please provide an argument."
+else
+  # Get the argument
+  input_string = ARGV[0]
+
+  # Define the regular expression pattern using Oniguruma syntax
+  regex_pattern = /hbt*n/
+
+  # Match the input against the regular expression
+  if input_string.match?(regex_pattern)
+    puts input_string.scan(regex_pattern).join
+  else
+    puts "No match found."
+  end
+end
