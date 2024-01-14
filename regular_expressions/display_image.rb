cedce5f10d6d8fbd6aea850e8f14df6ba1a877f1
danielmarial <d.kudum@alustudent.com>
2024-01-14 13:50:15 -0800

    Regex

--- regular_expressions/display_image.rb
+++
@@ -1,6 +0,0 @@
-require 'base64'
-
-image_file = File.read("9-passed_linkedin_regex_challenge.jpg")
-image_base64 = Base64.strict_encode64(image_file)
-
-puts "<img src='data:image/jpg;base64,#{image_base64}'>"
