22645ed0ed9c59161d267baa9ec499ab8705ab96
PMayala <p.mayala@alustudent.com>
2024-03-07 09:24:20 -0800

    1-top_ten.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,31 +1,17 @@
 #!/usr/bin/python3
-"""Module"""
+"""Return the number of subscribers of a given subreddit"""
 
-import json
 import requests
-import sys
 
 
 def number_of_subscribers(subreddit):
-    """Function that return the number of subscribers from REDDIT API"""
+    """function that fetches number_of_subscribers"""
+    URL = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    HEADERS = {"User-Agent": "PostmanRuntime/7.35.0"}
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "MyRedditBot/1.0 (by YourUsername)"}
+    try:
+        RESPONSE = requests.get(URL, headers=HEADERS, allow_redirects=False)
+        return RESPONSE.json().get("data").get("subscribers")
 
-    response = requests.get(url, headers=headers)
-
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
-    else:
+    except Exception:
         return 0
-
-
-if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        subreddit = sys.argv[1]
-        num_subscribers = number_of_subscribers(subreddit)
-        print(num_subscribers)
