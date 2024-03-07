be246eec0c69bc28c2f3f6dbff82ac49d15dd326
PMayala <p.mayala@alustudent.com>
2024-03-07 13:29:55 +0000

    0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -1,26 +1,21 @@
 #!/usr/bin/python3
 """Module"""
 
-import json
 import requests
 import sys
 
-
 def number_of_subscribers(subreddit):
-    """Function that return the number of subscribers from REDDIT API"""
+    """Function that returns the number of subscribers from the Reddit API"""
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "MyRedditBot/1.0 (by YourUsername)"}
-
     response = requests.get(url, headers=headers)
 
-    if response.status_code == 200:
+    if response.ok:
         data = response.json()
-        subscribers = data["data"]["subscribers"]
-        return subscribers
+        return data.get("data", {}).get("subscribers", 0)
     else:
         return 0
-
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
